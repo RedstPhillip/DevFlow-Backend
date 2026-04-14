e373c65382f577c53013f65c88a08e125be50d99
RedstPhillip <philliplutz01@gmail.com>
2026-04-14 09:58:09 +0200

    init spring boot

---
+++ src/main/java/com/example/devflowbackend/DevFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.devflowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevFlowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devflowbackend/DevFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devflowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
